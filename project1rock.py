1e3e7b0e4f4a672d6b367824f326875b06b1e1a3
Anam Shaikh <147983719+Anam123s@users.noreply.github.com>
2024-04-13 17:05:33 +0530

    Add files via upload

---
+++ project1rock.py
@@ -0,0 +1,25 @@
+import random
+
+rock='✊'
+paper='🫲'
+scissor='✌️'
+game_images=[rock,paper,scissor]
+user_choice=int(input("Enter Your choice Rock(0) Paper(1) Scissor(2) :"))
+
+if user_choice>=3 or user_choice<0:
+    print("Enter a valid choice")
+else:
+    print(f"You had entered {game_images[user_choice]}")
+    comp_choice=random.randint(0,2)
+    print(f"Computer choice is {game_images[comp_choice]}")
+
+    if(comp_choice==0 and user_choice==2):
+        print("You loose")
+    elif(user_choice==0 and comp_choice==2):
+        print("You win")
+    elif(comp_choice>user_choice):
+        print("You loose")
+    elif(user_choice>comp_choice):
+        print("You win")
+    elif(user_choice==comp_choice):
+        print("It's a draw")
